61e5d632925776b2c392fe9d6387e737592b7db7
David Stainton <dstainton415@gmail.com>
2016-12-24 14:03:10 +0000

    Fix multi_exp_on and silence warnings

--- src/crypto_primitives.rs
+++ src/crypto_primitives.rs
@@ -15,7 +15,7 @@ pub struct GroupCurve25519 {
 }
 
 impl GroupCurve25519 {
-    fn new() -> GroupCurve25519 {
+    pub fn new() -> GroupCurve25519 {
         GroupCurve25519 {
             g: [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -28,8 +28,9 @@ impl GroupCurve25519 {
     }
 
     /// Perform accumulating multiplication for each scalar
+    /// curve25519 function signature --> pub fn curve25519(n: &[u8], p: &[u8]) -> [u8; 32]
     pub fn multi_exp_on(self, base: &[u8; CURVE25519_SIZE], n: &[&[u8]]) -> [u8; CURVE25519_SIZE] {
-        n.iter().fold(*base, |acc, x| curve25519(&acc, x))
+        n.iter().fold(*base, |acc, x| curve25519(x, &acc))
     }
 }
 
@@ -37,13 +38,13 @@ impl GroupCurve25519 {
 mod tests {
     extern crate rustc_serialize;
     use super::*;
-    use self::rustc_serialize::hex::{FromHex,ToHex};
-    use crypto::curve25519::{Fe, curve25519};
+    //use self::rustc_serialize::hex::{FromHex,ToHex};
+    use self::rustc_serialize::hex::FromHex;
 
     #[test]
     fn commutativity_test() {
-        let mut curve1 = "82c8ad63392a5f59347b043e1244e68d52eb853921e2656f188d33e59a1410b4".from_hex().unwrap();
-        let mut curve2 = "4171bd9a48a58cf7579e9fa662fe0ac2acb8c6eed3056cd970fd35dd4d026cae".from_hex().unwrap();
+        let curve1 = "82c8ad63392a5f59347b043e1244e68d52eb853921e2656f188d33e59a1410b4".from_hex().unwrap();
+        let curve2 = "4171bd9a48a58cf7579e9fa662fe0ac2acb8c6eed3056cd970fd35dd4d026cae".from_hex().unwrap();
 
         let group = GroupCurve25519::new();
         let generator = group.g;
@@ -58,7 +59,6 @@ mod tests {
 
         let keys: &[&[u8]] = &[&curve1, &curve2];
         let fu = group.multi_exp_on(&generator, &keys);
-
         assert_eq!(fu, exp1);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,6 @@
 extern crate arrayref;
 extern crate crypto;
 
-use crypto::digest::Digest;
-use crypto::blake2b::Blake2b;
-use crypto::symmetriccipher::SynchronousStreamCipher;
-use crypto::chacha20::ChaCha20;
-
 pub mod crypto_primitives;
 pub use crypto_primitives::GroupCurve25519;
 
