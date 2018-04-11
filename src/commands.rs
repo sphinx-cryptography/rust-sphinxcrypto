961f0e23309d588334281891a1750c2c68a834f8
David Stainton <dstainton415@gmail.com>
2018-04-11 07:05:46 +0000

    Add from_bytes test; unfinished
    how do we can the value behind the trait object?

--- src/commands.rs
+++ src/commands.rs
@@ -1,14 +1,29 @@
 // commands.rs - sphinx cryptographic packet format commands
 // Copyright (C) 2018  David Stainton.
 
-use super::constants::{NODE_ID_SIZE};
+use super::constants::{NODE_ID_SIZE, RECIPIENT_ID_SIZE, SURB_ID_SIZE};
 use super::internal_crypto::{MAC_SIZE};
 
-/// length of the next hop command
+/// size of the next hop command in bytes
 const NEXT_HOP_SIZE: usize = 1 + NODE_ID_SIZE + MAC_SIZE;
 
+/// size of the recipient command in bytes
+const RECIPIENT_SIZE: usize = 1 + RECIPIENT_ID_SIZE;
+
+/// size of the SURB reply command in bytes
+const SURB_REPLY_SIZE: usize = 1 + SURB_ID_SIZE;
+
+/// size of the delay command in bytes
+const DELAY_SIZE: usize = 1 + 4;
+
 /// Sphinx routing commands.
-const NEXT_HOP: u8 = 0x1;
+const NULL_CMD: u8 = 0x0;
+const NEXT_HOP_CMD: u8 = 0x1;
+const RECIPIENT_CMD: u8 = 0x2;
+const SURB_REPLY_CMD: u8 = 0x3;
+
+/// Implementation defined commands.
+const DELAY_CMD: u8 = 0x80;
 
 /// RoutingCommand is a trait representing
 /// Sphinx routing commands
@@ -20,19 +35,20 @@ pub trait RoutingCommand {
 /// routing command and the rest of the buffer.
 pub fn from_bytes(b: &[u8]) -> Result<(Box<RoutingCommand>, Vec<u8>), &'static str> {
     let cmd_id = b[0];
-    let mut c = Vec::new();
-    c.clone_from_slice(&b[1..]);
     match cmd_id {
-        NextHop => {
+        NEXT_HOP_CMD => {
             let mut id = [0u8; NODE_ID_SIZE];
-            id.copy_from_slice(&c[..NODE_ID_SIZE]);
+            id.copy_from_slice(&b[1..NODE_ID_SIZE+1]);
             let mut mac = [0u8; MAC_SIZE];
-            mac.clone_from_slice(&c[NODE_ID_SIZE..NODE_ID_SIZE+MAC_SIZE]);
+            mac.clone_from_slice(&b[1+NODE_ID_SIZE..NODE_ID_SIZE+MAC_SIZE+1]);
             let next_hop = NextHop{
                 id: id,
                 mac: mac,
             };
-            return Ok((Box::new(next_hop), b[NEXT_HOP_SIZE..].to_vec()))
+            return Ok((Box::new(next_hop), b[NEXT_HOP_SIZE..].to_vec()));
+        }
+        _ => {
+            return Err("error failed to decode command(s) from bytes");
         }
     }
     Err("error failed to decode command(s) from bytes")
@@ -47,7 +63,7 @@ pub struct NextHop {
 impl RoutingCommand for NextHop {
     fn to_vec(&self) -> Vec<u8> {
         let mut out = Vec::new();
-        out.push(NEXT_HOP);
+        out.push(NEXT_HOP_CMD);
         out.extend_from_slice(&self.id);
         out.extend_from_slice(&self.mac);
         out
@@ -69,16 +85,46 @@ fn next_hop_from_bytes(b: &[u8]) -> Result<(NextHop, Vec<u8>), &'static str> {
     return Ok((cmd, b[NEXT_HOP_SIZE-1..].to_vec()))
 }
 
+#[derive(Copy,Clone)]
+pub struct Recipient {}
+
+
 
 #[cfg(test)]
 mod tests {
     extern crate rand;
+    extern crate rustc_serialize;
+
     use super::*;
     use self::rand::Rng;
     use self::rand::os::OsRng;
+    use self::rustc_serialize::hex::ToHex;
 
     use super::super::constants::{NODE_ID_SIZE};
     use super::super::internal_crypto::{MAC_SIZE};
+
+    #[test]
+    fn from_bytes_test() {
+
+        // next_hop case
+        let mut rnd = OsRng::new().unwrap();
+
+        let id = rnd.gen_iter::<u8>().take(NODE_ID_SIZE).collect::<Vec<u8>>();
+        let mut idArr = [0u8; NODE_ID_SIZE];
+        idArr.copy_from_slice(id.as_slice());
+
+        let mac = rnd.gen_iter::<u8>().take(MAC_SIZE).collect::<Vec<u8>>();
+        let mut macArr = [0u8; MAC_SIZE];
+        macArr.copy_from_slice(mac.as_slice());
+
+        let cmd = NextHop{
+            id: idArr,
+            mac: macArr,
+        };
+        let raw = cmd.to_vec();
+        let (boxed_cmd, rest) = from_bytes(&raw).unwrap();
+        // XXX how to get NextHop pointer from (*Box::into_raw(boxed_cmd))
+    }
     
     #[test]
     fn next_hop_test() {
@@ -96,9 +142,9 @@ mod tests {
             id: idArr,
             mac: macArr,
         };
-        let raw1 = cmd1.to_bytes();
+        let raw1 = cmd1.to_vec();
         let (cmd, rest) = super::next_hop_from_bytes(&raw1[1..]).unwrap();     
-        let raw2 = cmd.to_bytes();
+        let raw2 = cmd.to_vec();
         assert_eq!(raw1, raw2);
     }
 }
