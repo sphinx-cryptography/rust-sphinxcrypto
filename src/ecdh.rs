f3a730b3ab0c5d94fdf7486b79993c88b48fa77f
David Stainton <dstainton415@gmail.com>
2018-05-24 01:46:11 +0000

    Update copyright and license notices

--- src/ecdh.rs
+++ src/ecdh.rs
@@ -1,4 +1,4 @@
-// ecdh.rs - wrapping library for curve25519 dh operations
+// ecdh.rs - wrapper for X25519 Diffie-Hellman and blinding operations
 // Copyright (C) 2018  David Anthony Stainton.
 //
 // MIT License
@@ -9,10 +9,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
